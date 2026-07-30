da537eca6e191215135dbe68dbaed91dddacec3a
lizhicheng <1lizhicheng00@gmail.com>
2026-07-30 11:03:50 +0800

    Remove pre-release quota backfill

--- gateway-tunnel-traffic-allowed.sql
+++ gateway-tunnel-traffic-allowed.sql
@@ -3,16 +3,6 @@ ALTER TABLE billing_account
     ADD COLUMN quota_blocked_until BIGINT UNSIGNED NOT NULL DEFAULT 0
         COMMENT 'traffic blocked before this unix second';
 
--- Preserve already exhausted accounts when upgrading.
-UPDATE billing_account a
-JOIN (
-    SELECT account_id, MAX(period_end) AS blocked_until
-    FROM billing_period
-    WHERE billed_bytes >= quota_bytes
-    GROUP BY account_id
-) exhausted ON exhausted.account_id = a._id
-SET a.quota_blocked_until = exhausted.blocked_until;
-
 -- Run after billing_period.billed_bytes is updated in the same settlement transaction.
 UPDATE billing_account a
 JOIN billing_period bp
